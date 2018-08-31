b107bde349b9037394e18e9c11545bf2a70864d5
Smith, Greg W <Greg.W.Smith@nordstrom.com>
2018-08-31 13:07:48 -0700

    Properly implements NPM command `refresh-lambda-hash`. Includes new .integrity.yml for assets version 0.0.1.

--- refresh-lambda-hash.js
+++ refresh-lambda-hash.js
@@ -1,29 +1,11 @@
 const fs = require('fs')
 const path = require('path')
+const yaml = require('js-yaml')
 const { hashElement } = require('folder-hash')
 
-const lambdaPath = path.join(__dirname, 'lib', 'lambda')
-const lambdaPackagePath = path.join(lambdaPath, 'package.json')
-const lambdaPackage = fs.readFileSync(lambdaPackagePath, 'utf-8')
-const lambdaPackageObject = JSON.parse(lambdaPackage)
-const existingIntegrity = lambdaPackageObject._integrity // eslint-disable-line no-underscore-dangle
+const integrity = require('./lib/integrity-check.js')(fs, path, yaml, hashElement)
 
-if (!existingIntegrity) {
-  console.log('ERROR: No existing integrity hash found.')
-  process.exit(1)
-}
+console.log('Generating updated integrity hash ...')
 
-const options = {
-  folders: { exclude: ['.serverless', 'node_modules'] },
-  files: { include: ['*.js'] },
-}
-
-console.log(`Generating updated integrity hash for: ${lambdaPath} ...`)
-
-hashElement(lambdaPath, options)
-  .then((hash) => {
-    const hashString = hash.hash
-    fs.writeFileSync(lambdaPackagePath, lambdaPackage.replace(existingIntegrity, hashString))
-    console.log(`Updated integrity hash: ${hashString}\nDONE.`)
-  })
-  .catch(error => console.error('hashing failed:', error))
+integrity.updateIntegrityFile(__dirname)
+  .then(() => console.log('Done.'))
