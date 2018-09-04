da03dd55d8a5294731e642fdbe36e5902ef03aa1
Smith, Greg W <Greg.W.Smith@nordstrom.com>
2018-09-04 15:26:05 -0700

    The refresh-lambda-hash script now consumes and passes semver-compare module.

--- refresh-lambda-hash.js
+++ refresh-lambda-hash.js
@@ -2,8 +2,9 @@ const fs = require('fs')
 const path = require('path')
 const yaml = require('js-yaml')
 const { hashElement } = require('folder-hash')
+const cmp = require('semver-compare')
 
-const integrity = require('./lib/integrity-check.js')(fs, path, yaml, hashElement)
+const integrity = require('./lib/integrity-check.js')(fs, path, yaml, hashElement, cmp)
 
 console.log('Generating updated integrity hash ...')
 
